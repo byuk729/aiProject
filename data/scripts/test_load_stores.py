ba3ddf7dff78f406e5bc9dae3cabdbdcb348d461
Annie Nguyen <nguyenannie221@gmail.com>
2026-03-27 16:59:10 -0400

    add script to test scores csv loading

---
+++ data/scripts/test_load_stores.py
@@ -0,0 +1,15 @@
+import csv
+
+file_path = "../clean/stores.csv"
+
+'''
+Opens stores.csv, stores it, prints header
+Confirms if path/file exists, is readable, etc.
+'''
+with open(file_path, newline="", encoding="utf-8") as csvfile:
+    reader = csv.reader(csvfile)
+    rows = list(reader)
+
+print("CSV loaded successfully.")
+print("Number of rows:", len(rows))
+print("Header:", rows[0])
